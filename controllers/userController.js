5604ba00479f1c3f324d67df6ed6ab6a06beeaa5
Timothy Ling <timothyling1994@gmail.com>
2021-07-10 17:41:13 -0700

    Implemented validation and sanitization for user creation workflow for /user API endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,39 @@ const { body,validationResult } = require('express-validator');
 var User = require('../models/user');
 
 
-exports.create_user = function (req,res,next){
-	
-	return res.send(req.body);
-};
+exports.create_user = [
+	body('email',"Must be an email address").isEmail().trim().escape().normalizeEmail(),
+	body('display_name',"Must be an email address").trim().escape(),
+	body('password').isLength({min:8}).withMessage('Password must be at least 8 characters long.')
+	.matches('[0-9]').withMessage('Password Must Contain a Number')
+	.matches('[A-Z]').withMessage('Password Must Contain an Uppercase Letter')
+	.trim().escape(),
+
+	(req,res,next) => {
+		const errors = validationResult(req);
+
+		if(!errors.isEmpty()){
+			res.status(422).json({
+				errors: errors.array()
+			});
+			return;
+		}
+
+		else
+		{
+			return res.json({
+				email: req.body.email,
+				password: req.body.password,
+				display_name: req.body.display_name,
+			});
+			//bcrypt.hash(req.body.password)
+
+			/*let user = new User({
+				email: req.body
+			});*/
+		}
+	}
+
+
+
+];
--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
 	email: {type:String,required:true},
-	username: {type:String, required:true},
+	display_name: {type:String, required:true},
 	password: {type:String,required:true},
 });
 
